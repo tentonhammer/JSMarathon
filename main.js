75cbe759654d0fa2ed646779eab53137546dba68
TenTonHammer <goowernor@gmail.com>
2021-10-14 19:51:10 +1000

    feat: task-0 completed

---
+++ main.js
@@ -0,0 +1,15 @@
+const Nikita = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['kunai', 'shuriken'],
+    attack: () => console.log(Nikita.name + ' Fight...')
+}
+
+const Eugene = {
+    name: 'Sub-Zero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: ['spear', 'axe'],
+    attack: () => console.log(Eugene.name + ' Fight...')
+}
