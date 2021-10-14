0c901ef720ea142ecc5f27fb747e81abd906a21d
TenTonHammer <goowernor@gmail.com>
2021-10-14 22:53:39 +1000

    feat: task-3 completed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const nikita = {
     name: 'Scorpion',
-    hp: 100,
+    hp: 23,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['kunai', 'shuriken'],
     attack: () => console.log(nikita.name + ' Fight...')
@@ -8,25 +8,33 @@ const nikita = {
 
 const eugene = {
     name: 'Sub-Zero',
-    hp: 100,
+    hp: 49,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['spear', 'axe'],
     attack: () => console.log(eugene.name + ' Fight...')
 }
 
-const createPlayer = () => {
-    let player1 = document.createElement('div');
-    player1.classList.add('player1');
+const createPlayer = (_className, {name, hp, img}) => {
+    let player = document.createElement('div');
+    player.classList.add(_className);
     let progress = document.createElement('div');
     progress.classList.add('progressbar');
     let character = document.createElement('div');
     character.classList.add('character');
-    let life = document.createElement('div');
-    life.classList.add('life');
-    life.style.width = '100%';
-    let name = document.createElement('div');
-    name.classList.add('name');
-    name.innerText = eugene.name;
-    let img = document.createElement('img');
-    img.src = 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif';
+    let divLife = document.createElement('div');
+    divLife.classList.add('life');
+    divLife.style.width = `${hp}%`;
+    let divName = document.createElement('div');
+    divName.classList.add('name');
+    divName.innerText = name;
+    let divImg = document.createElement('img');
+    divImg.src = img;
+    player.append(progress, character);
+    progress.append(divLife, divName);
+    character.append(divImg);
+    let arena = document.getElementsByClassName('arenas');
+    arena.item(0).append(player);
 }
+
+createPlayer('player1', nikita);
+createPlayer('player2', eugene);
