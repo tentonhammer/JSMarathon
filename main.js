f3fa73adb319de0d1878be1c8d1888479f1bb1fa
TenTonHammer <goowernor@gmail.com>
2021-10-14 22:39:03 +1000

    feat: task-1 completed

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-const Nikita = {
+const nikita = {
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['kunai', 'shuriken'],
-    attack: () => console.log(Nikita.name + ' Fight...')
+    attack: () => console.log(nikita.name + ' Fight...')
 }
 
-const Eugene = {
+const eugene = {
     name: 'Sub-Zero',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['spear', 'axe'],
-    attack: () => console.log(Eugene.name + ' Fight...')
+    attack: () => console.log(eugene.name + ' Fight...')
+}
+
+const createPlayer = () => {
+    let player1 = document.createElement('div');
+    player1.classList.add('player1');
+    let progress = document.createElement('div');
+    progress.classList.add('progressbar');
+    let character = document.createElement('div');
+    character.classList.add('character');
+    let life = document.createElement('div');
+    life.classList.add('life');
+    life.style.width = '100%';
+    let name = document.createElement('div');
+    name.classList.add('name');
+    name.innerText = eugene.name;
+    let img = document.createElement('img');
+    img.src = 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif';
 }
