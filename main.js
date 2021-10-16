72ccdbb0c743d9d568cec2f8970e98ce40e23a3b
TenTonHammer <goowernor@gmail.com>
2021-10-16 21:44:04 +1000

    ref: code refactoring

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const arena = document.getElementsByClassName('arenas').item(0);
+
 const nikita = {
+    player: 1,
     name: 'Scorpion',
     hp: 23,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -7,6 +10,7 @@ const nikita = {
 }
 
 const eugene = {
+    player: 2,
     name: 'Sub-Zero',
     hp: 49,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
@@ -14,28 +18,30 @@ const eugene = {
     attack: () => console.log(eugene.name + ' Fight...')
 }
 
-const arena = document.getElementsByClassName('arenas').item(0);
-
-const createPlayer = (_className, {name, hp, img}) => {
-    let player = document.createElement('div');
-    player.classList.add(_className);
-    let progress = document.createElement('div');
-    progress.classList.add('progressbar');
-    let character = document.createElement('div');
-    character.classList.add('character');
-    let divLife = document.createElement('div');
-    divLife.classList.add('life');
-    divLife.style.width = `${hp}%`;
-    let divName = document.createElement('div');
-    divName.classList.add('name');
-    divName.innerText = name;
-    let divImg = document.createElement('img');
-    divImg.src = img;
-    player.append(progress, character);
-    progress.append(divLife, divName);
-    character.append(divImg);
-    arena.append(player);
+const createElement = (tagName, className) => {
+    const tag = document.createElement(tagName);
+    if (className) {
+        tag.classList.add(className)
+    }
+    return tag;
 }
 
-createPlayer('player1', nikita);
-createPlayer('player2', eugene);
+const createPlayer = ({player, name, hp, img}) => {
+    let divPlayer = createElement('div', `player${player}`);
+    let progress = createElement('div', 'progressbar');
+    let character = createElement('div', 'character');
+    let divLife = createElement('div', 'life');
+    divLife.style.width = `${hp}%`;
+    let divName = createElement('div', 'name');
+    divName.innerText = name;
+    let divImg = createElement('img');
+    divImg.src = img;
+    divPlayer.append(progress, character);
+    progress.append(divLife, divName);
+    character.append(divImg);
+
+    return divPlayer;
+}
+
+arena.append(createPlayer(nikita));
+arena.append(createPlayer(eugene));
